ca24f89b8b658600cdc6ca9faeae724fb1944916
David Brister <davidmurdockbrister@gmail.com>
2020-11-05 17:59:11 -0400

    test

--- src/resources/js/components/App.js
+++ src/resources/js/components/App.js
@@ -3,7 +3,7 @@
     import React, { Component } from 'react'
     import ReactDOM from 'react-dom'
     import { BrowserRouter, Route, Switch } from 'react-router-dom'
-    import Header from './Header'
+    import Header from './Headgit remote adder'
     import CourseList from './CourseList'
     import Course from './Course'
 
