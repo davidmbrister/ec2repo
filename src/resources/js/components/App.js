2d30570fc5dae56c683dd56e0b7638c8f6c0174d
David Brister <davidmurdockbrister@gmail.com>
2020-11-09 16:01:51 -0400

    added react-spectrum

--- src/resources/js/components/App.js
+++ src/resources/js/components/App.js
@@ -6,23 +6,24 @@
     import Header from './Header'
     import CourseList from './CourseList'
     import Course from './Course'
+    import {Provider, defaultTheme, Button, Meter} from '@adobe/react-spectrum';
 
     class App extends Component {
       render () {
         return (
-          <BrowserRouter>
-            <div className="root-app-div">
-              <TextField label="Name" />
-               <p>Somethingg to change and show up on server</p> 
-              <Header />
-              {/* Course list does not currently take any props, it's the same data for any user at any time */}
-            </div>
-{/*             <Course
-              // a test to show that card works on its own
-              basicData={{"id":1,"user_id":1,"category_id":1,"title":"jghfj","description":"fghj","photo":null,"video":null,"status":1,"what_will_student_learn":"fghjfhj","age_group":"ages 8-12","capacity":2,"discount_price":34.12,"standard_price":34.23,"subscriber_count":0,"tags":"PHP, Laravel","created_at":"2020-09-11T21:47:21.000000Z","updated_at":"2020-09-11T21:50:32.000000Z","deleted_at":null},{"id":2,"user_id":1,"category_id":2,"title":"Minecraft","description":"A camp for kids to learn scientific concepts on a Minecraft server supervised by a camp director.","photo":null,"video":null,"status":1,"what_will_student_learn":"Cooperation, Science","age_group":"ages 8-12","capacity":19,"discount_price":23.14,"standard_price":26,"subscriber_count":0,"tags":"Cooperation, Science","created_at":"2020-09-11T22:13:43.000000Z","updated_at":"2020-09-11T22:13:43.000000Z","deleted_at":null}}
-            /> */}
-            <CourseList />
-          </BrowserRouter>
+          <Provider theme={defaultTheme}>
+              <BrowserRouter>
+                <div className="root-app-div">
+                  <p>Somethingg to change and show up on server</p> 
+                  <Header />
+                  {/* Course list does not currently take any props, it's the same data for any user at any time */}
+                </div>
+                <Meter label="Seats taken" variant="positive" value={75} />
+                
+                <Button variant="cta" margin="size-160">Get started</Button>
+                <CourseList />
+              </BrowserRouter>
+          </Provider>
         )
       }
     }
